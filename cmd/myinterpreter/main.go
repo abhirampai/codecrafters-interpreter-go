d7cdd3aae7255993a500d857c45ef6d29befec3e
Abhiram Pai <abhirampai1999@gmail.com>
2024-11-20 19:55:53 +0530

    Passing stage assignment and equality operators

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -29,41 +29,48 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
-	
+
 	if len(fileContents) > 0 {
 		for i := 0; i < len(fileContents); i++ {
 			text := string(fileContents[i])
 			switch fileContents[i] {
-				case '(':
-					addToken("LEFT_PAREN", text);
-				case ')':
-					addToken("RIGHT_PAREN", text);
-				case '{':
-					addToken("LEFT_BRACE", text);
-				case '}':
-					addToken("RIGHT_BRACE", text);	
-				case ',':
-					addToken("COMMA", ",");
-				case '.':
-					addToken("DOT", ".");
-				case '+':
-					addToken("PLUS", "+");
-				case '-':
-					addToken("MINUS", "-");
-				case '*':
-					addToken("STAR", "*");
-				case ';':
-					addToken("SEMICOLON", ";");
-				default:
-					fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %c\n", fileContents[i]);
-					has_errors = true;
+			case '(':
+				addToken("LEFT_PAREN", text)
+			case ')':
+				addToken("RIGHT_PAREN", text)
+			case '{':
+				addToken("LEFT_BRACE", text)
+			case '}':
+				addToken("RIGHT_BRACE", text)
+			case ',':
+				addToken("COMMA", ",")
+			case '.':
+				addToken("DOT", ".")
+			case '+':
+				addToken("PLUS", "+")
+			case '-':
+				addToken("MINUS", "-")
+			case '*':
+				addToken("STAR", "*")
+			case ';':
+				addToken("SEMICOLON", ";")
+			case '=':
+				if i+1 < len(fileContents) && fileContents[i+1] == '=' {
+					addToken("EQUAL_EQUAL", "==")
+					i += 1
+				} else {
+					addToken("EQUAL", "=")
+				}
+			default:
+				fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %c\n", fileContents[i])
+				has_errors = true
 			}
 		}
 		fmt.Println("EOF  null")
 	} else {
 		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}
-	
+
 	if has_errors {
 		os.Exit(65)
 	}
